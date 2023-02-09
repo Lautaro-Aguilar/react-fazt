a6237173bc2e925db2a48638d0d46f8f3c2cd725
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-09 19:59:37 -0300

    react props

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,15 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
-import Product, {NavBar} from "./Product";
+import Product, { NavBar } from "./Product";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Greeting />
-    <UserCard />
-    <Product />
-    <NavBar />
+    <UserCard
+      name="Ryan Ray"
+      amount={3000}
+      married={true}
+      points={[99, 33.3, 22.2]}
+      address={{ street: "123 Main Street", city: "New York" }}
+      greet={function () {
+        alert("hello");
+      }}
+    />
   </>
 );
