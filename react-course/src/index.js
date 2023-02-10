5dc14b930642fc3acb3dbe68430cf2ef0c76201f
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-10 12:10:32 -0300

    Arrays en React

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -8,8 +8,28 @@ import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const users = [
+  {
+    id: 1,
+    name: "Ryan Ray",
+    image: "https://robohash.org/user1",
+  },
+  {
+    id: 2,
+    name: "Joe",
+    image: "https://robohash.org/user2",
+  },
+];
+
 root.render(
   <>
-    <Posts />
+    {users.map((user, i) => {
+      return (
+        <div key={i}>
+          <h1>{user.name}</h1>
+          <img src={user.image} />
+        </div>
+      );
+    })}
   </>
 );
