d09c74e770ca80066fc18590469a3deddc2add5b
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-10 12:49:25 -0300

    useEffect
    Mandar mensaje a la consola cuando un componente se creó/cargo

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { NavBar } from "./Product";
@@ -10,6 +10,8 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
   const [mensaje, setMensaje] = useState("");
+
+  useEffect(() => {console.log("render")},[]);
 
   return (
     <div>
