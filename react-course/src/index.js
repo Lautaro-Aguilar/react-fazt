d0b7c3fe401ace1f9ee9cd4bb5726deae6a71047
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-10 11:40:39 -0300

    Fetch API

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -4,27 +4,12 @@ import { Greeting, UserCard } from "./Greeting";
 import Product, { NavBar } from "./Product";
 import { Button } from "./Button";
 import { TaskCard } from "./Task";
+import { Posts } from "./Posts";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <TaskCard />
-    <Button text="saludar" />
-
-    <form
-      onSubmit={(e) => {
-        e.preventDefault();
-        alert("enviado");
-      }}
-    >
-      <h1>Registro de usuario</h1>
-      <button>Send</button>
-    </form>
-    <input
-      onChange={(e) => {
-        console.log(e.target.value);
-      }}
-    ></input>
+    <Posts />
   </>
 );
