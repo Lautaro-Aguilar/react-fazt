06c64115d43870f454ab5073dda29c39ab917b54
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-10 12:53:25 -0300

    useEffect aplicado a un contador
    Escribir render cada vez que se utiliza el contador

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -10,14 +10,28 @@ const root = ReactDOM.createRoot(document.getElementById("root"));
 
 function Counter() {
   const [mensaje, setMensaje] = useState("");
+  const [counter, setCounter] = useState(0);
 
-  useEffect(() => {console.log("render")},[]);
+  useEffect(() => {
+    console.log("render");
+  }, [counter]);
 
   return (
     <div>
       <input onChange={(e) => setMensaje(e.target.value)} />
 
       <button onClick={() => alert("El mensaje es: " + mensaje)}>Save</button>
+
+      <hr />
+
+      <h1>Counter: {counter} </h1>
+      <button
+        onClick={() => {
+          setCounter(counter + 1);
+        }}
+      >
+        Incrementar
+      </button>
     </div>
   );
 }
