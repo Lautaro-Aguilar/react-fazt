f8591e91585ff30a0d5ff0711a37403c72588b6e
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-10 06:55:23 -0300

    update files

--- react-course/src/index.js
+++ react-course/src/index.js
@@ -3,23 +3,12 @@ import ReactDOM from "react-dom/client";
 import { Greeting, UserCard } from "./Greeting";
 import Product, { NavBar } from "./Product";
 import { Button } from "./Button";
+import { TaskCard } from "./Task";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
 root.render(
   <>
-    <Button text="Click Me"/>
-    <Button text="Pay"/>
-    <Button text='' name="Joe"/>
-    {/*     <UserCard
-      name="Ryan Ray"
-      amount={3000}
-      married={true}
-      points={[99, 33.3, 22.2]}
-      address={{ street: "123 Main Street", city: "New York" }}
-      greet={function () {
-        alert("hello");
-      }}
-    /> */}
+    <TaskCard/>
   </>
 );
