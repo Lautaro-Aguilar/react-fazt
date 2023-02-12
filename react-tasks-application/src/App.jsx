3ab5eb24be298bc1760b154f03cf18edadcbe9b3
zSupAnonymo <aguilarlautaroexequiel867@gmail.com>
2023-02-12 20:39:52 -0300

    añadir tareas

--- react-tasks-application/src/App.jsx
+++ react-tasks-application/src/App.jsx
@@ -1,11 +1,29 @@
-import TaskList from './TaskList'
+import TaskList from "./TaskList";
+import TaskForm from "./TaskForm";
+import { tasks as data } from "./tasks";
+import { useState, useEffect } from "react";
 
-function holamundo() {
+function App() {
+  const [tasks, setTasks] = useState([]);
+
+  useEffect(() => {
+    setTasks(data);
+  }, []);
+
+  function createTask(taskTitle) {
+    setTasks([...tasks, {
+      title: taskTitle,
+      id: tasks.length,
+      description: "nueva tarea"
+    }]);
+  }
+
   return (
-    <div>
-      <TaskList/>
-    </div>
-  )
+    <>
+      <TaskForm createTask={createTask}/>
+      <TaskList tasks={tasks} />
+    </>
+  );
 }
 
-export default holamundo
+export default App;
